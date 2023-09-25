63a03e39f7d52972fdfd0e3cee6468aa5eba5844
SAIRAM <ramphilo@gmail.com>
2023-09-25 14:13:47 +0530

    Deleted
    Deleted

--- Day 9/src/Admin/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Ecommerce } from './Ecommerce';
-export { default as Orders } from './Orders';
-export { default as Employees } from './Employees';
-export { default as Customers } from './Customers';
